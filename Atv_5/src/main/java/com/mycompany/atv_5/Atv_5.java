cb827c4a0ea878c3ba65acd99f099b5398db0c4e
Pedro H. <96092387+Fenizi@users.noreply.github.com>
2024-09-20 20:30:45 -0300

    Add files via upload

---
+++ Atv_5/src/main/java/com/mycompany/atv_5/Atv_5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atv_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Atv_5 {
+
+    public static void main(String[] args) {
+   
+        String meuSobrenome = "Sena Mendes";
+        String sobrenome;
+        
+        Scanner ler = new Scanner(System.in);
+        
+        
+        System.out.print("Digite um sobrenome: ");
+        sobrenome = ler.nextLine();
+        
+        if (!sobrenome.equalsIgnoreCase(meuSobrenome)) {
+            System.out.println("O sobrenome digitado é diferente do seu.");
+        } else {
+            System.out.println("O sobrenome digitado é igual ao seu.");
+        }
+    }
+}
+  
